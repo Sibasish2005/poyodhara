4528ce0505644ba72c11f82fa737da576bbfb118
sibasish005 <sibasishchakraborti@gmail.com>
2026-04-24 21:15:11 +0530

    feat: add WhatsApp button component for direct messaging

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import WhatsAppButton from "./components/whatappButton";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -58,6 +59,7 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col overflow-x-hidden">
         {children}
+        <WhatsAppButton/>
       </body>
     </html>
   );
